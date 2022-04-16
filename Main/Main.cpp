170139f851c4a28a202ced6423aa0360c36fe163
yang-shuohao <2499134263@qq.com>
2022-04-16 18:11:24 +0800

    更新README

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,9 +1,259 @@
 #include<Windows.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <wrl.h>
+#include<iostream>
+
+#include "../DXUtils/d3dx12.h"
+
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+using namespace Microsoft::WRL;
+
+
+const UINT FrameCount = 2;
+UINT width = 800;
+UINT height = 600;
+HWND hwnd;
+
+// Pipeline objects.
+ComPtr<IDXGISwapChain3> swapChain;
+ComPtr<ID3D12Device> device;
+ComPtr<ID3D12Resource> renderTargets[FrameCount];
+ComPtr<ID3D12CommandAllocator> commandAllocator;
+ComPtr<ID3D12CommandQueue> commandQueue;
+ComPtr<ID3D12DescriptorHeap> rtvHeap;
+ComPtr<ID3D12PipelineState> pipelineState;
+ComPtr<ID3D12GraphicsCommandList> commandList;
+UINT rtvDescriptorSize;
+
+// Synchronization objects.
+UINT frameIndex;
+HANDLE fenceEvent;
+ComPtr<ID3D12Fence> fence;
+UINT64 fenceValue;
+
+
+inline std::string HrToString(HRESULT hr)
+{
+	char s_str[64] = {};
+	sprintf_s(s_str, "HRESULT of 0x%08X", static_cast<UINT>(hr));
+	return std::string(s_str);
+}
+
+class HrException : public std::runtime_error
+{
+public:
+	HrException(HRESULT hr) : std::runtime_error(HrToString(hr)), m_hr(hr) {}
+	HRESULT Error() const { return m_hr; }
+private:
+	const HRESULT m_hr;
+};
+
+inline void ThrowIfFailed(HRESULT hr)
+{
+	if (FAILED(hr))
+	{
+		throw HrException(hr);
+	}
+}
+
+IDXGIAdapter1* GetSupportedAdapter(Microsoft::WRL::ComPtr<IDXGIFactory4>& dxgiFactory,const D3D_FEATURE_LEVEL featureLevel)
+{
+	IDXGIAdapter1* adapter = nullptr;
+	for (std::uint32_t adapterIndex = 0U; ; ++adapterIndex) 
+	{
+		IDXGIAdapter1* currentAdapter = nullptr;
+		if (DXGI_ERROR_NOT_FOUND == dxgiFactory->EnumAdapters1(adapterIndex, &currentAdapter)) 
+		{
+			break;
+		}
+
+		const HRESULT hres = D3D12CreateDevice(currentAdapter,featureLevel,_uuidof(ID3D12Device),nullptr);
+		if (SUCCEEDED(hres)) 
+		{
+			adapter = currentAdapter;
+			break;
+		}
+
+		currentAdapter->Release();
+	}
+
+	return adapter;
+}
+
+void LoadPipeline()
+{
+
+#if defined(_DEBUG)
+	{
+		ComPtr<ID3D12Debug> debugController;
+		if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+		{
+			debugController->EnableDebugLayer();
+		}
+	}
+#endif 
+
+	ComPtr<IDXGIFactory4> mDxgiFactory;
+	ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(mDxgiFactory.GetAddressOf())));
+
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_12_1,
+		D3D_FEATURE_LEVEL_12_0,
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+	};
+
+	IDXGIAdapter1* adapter = nullptr;
+	for (std::uint32_t i = 0U; i < _countof(featureLevels); ++i)
+	{
+		adapter = GetSupportedAdapter(mDxgiFactory, featureLevels[i]);
+		if (adapter != nullptr) 
+		{
+			break;
+		}
+	}
+
+	if (adapter != nullptr)
+	{
+		D3D12CreateDevice(adapter,D3D_FEATURE_LEVEL_12_1,IID_PPV_ARGS(device.GetAddressOf()));
+	}
+
+	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+	queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+
+	ThrowIfFailed(device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&commandQueue)));
+
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+	swapChainDesc.BufferCount = FrameCount;
+	swapChainDesc.Width = width;
+	swapChainDesc.Height = height;
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	swapChainDesc.SampleDesc.Count = 1;
+
+	ComPtr<IDXGISwapChain1> swapChain1;
+	ThrowIfFailed(mDxgiFactory->CreateSwapChainForHwnd(
+		commandQueue.Get(),  
+		hwnd,
+		&swapChainDesc,
+		nullptr,
+		nullptr,
+		&swapChain1
+	));
+
+	ThrowIfFailed(swapChain1.As(&swapChain));
+	frameIndex = swapChain->GetCurrentBackBufferIndex();
+
+	{
+		D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+		rtvHeapDesc.NumDescriptors = FrameCount;
+		rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+		rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+		ThrowIfFailed(device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&rtvHeap)));
+
+		rtvDescriptorSize = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	}
+
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(rtvHeap->GetCPUDescriptorHandleForHeapStart());
+
+	for (UINT n = 0; n < FrameCount; n++)
+	{
+		ThrowIfFailed(swapChain->GetBuffer(n, IID_PPV_ARGS(&renderTargets[n])));
+		device->CreateRenderTargetView(renderTargets[n].Get(), nullptr, rtvHandle);
+		rtvHandle.Offset(1, rtvDescriptorSize);
+	}
+
+	ThrowIfFailed(device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator)));
+
+}
+
+void LoadAsset()
+{
+	ThrowIfFailed(device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator.Get(), nullptr, IID_PPV_ARGS(&commandList)));
+
+	ThrowIfFailed(commandList->Close());
+
+	{
+		ThrowIfFailed(device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence)));
+		fenceValue = 1;
+
+		fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+		if (fenceEvent == nullptr)
+		{
+			ThrowIfFailed(HRESULT_FROM_WIN32(GetLastError()));
+		}
+	}
+}
+
+void PopulateCommandList()
+{
+	ThrowIfFailed(commandAllocator->Reset());
+	ThrowIfFailed(commandList->Reset(commandAllocator.Get(), pipelineState.Get()));
+
+	D3D12_RESOURCE_BARRIER resBarrier = CD3DX12_RESOURCE_BARRIER::Transition(renderTargets[frameIndex].Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
+	commandList->ResourceBarrier(1, &resBarrier);
+
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(rtvHeap->GetCPUDescriptorHandleForHeapStart(), frameIndex, rtvDescriptorSize);
+
+	const float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
+	commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
+
+	resBarrier = CD3DX12_RESOURCE_BARRIER::Transition(renderTargets[frameIndex].Get(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
+	commandList->ResourceBarrier(1, &resBarrier);
+
+	ThrowIfFailed(commandList->Close());
+}
+
+void WaitForPreviousFrame()
+{
+	const UINT64 tempFenceValue = fenceValue;
+	ThrowIfFailed(commandQueue->Signal(fence.Get(), tempFenceValue));
+	fenceValue++;
+
+	if (fence->GetCompletedValue() < tempFenceValue)
+	{
+		ThrowIfFailed(fence->SetEventOnCompletion(tempFenceValue, fenceEvent));
+		WaitForSingleObject(fenceEvent, INFINITE);
+	}
+
+	frameIndex = swapChain->GetCurrentBackBufferIndex();
+}
+
+
+void OnRender()
+{
+	PopulateCommandList();
+
+	ID3D12CommandList* ppCommandLists[] = { commandList.Get() };
+	commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
+
+	ThrowIfFailed(swapChain->Present(1, 0));
+
+	WaitForPreviousFrame();
+}
+
+void OnDestroy()
+{
+	WaitForPreviousFrame();
+
+	CloseHandle(fenceEvent);
+}
+
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
+	case WM_PAINT:
+		OnRender();
+		return 0;
 
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -22,24 +272,26 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	windowClass.hInstance = hInstance;
 	windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	windowClass.lpszClassName = L"YSHRenderClass";
-	
+
 	RegisterClassEx(&windowClass);
 
-	HWND hwnd = CreateWindow(
+	hwnd = CreateWindow(
 		windowClass.lpszClassName,
 		L"YSHRender",
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
-		800,
-		600,
-		nullptr,       
-		nullptr,       
+		width,
+		height,
+		nullptr,
+		nullptr,
 		hInstance,
 		nullptr);
 
-	ShowWindow(hwnd, SW_SHOW);
+	LoadPipeline();
+	LoadAsset();
 
+	ShowWindow(hwnd, SW_SHOW);
 
 	MSG msg = {};
 	while (msg.message != WM_QUIT)
@@ -51,6 +303,7 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 		}
 	}
 
+	OnDestroy();
 
 	return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # YSHRender
 YSHRender是个人学习渲染技术开发的一个渲染引擎，重点在于通过这个渲染引擎学习前沿的渲染技术。
+
+# DX12渲染引擎
+[DX12渲染引擎（零）开篇](https://zhuanlan.zhihu.com/p/499521948)
+[DX12渲染引擎（一）创建项目](https://zhuanlan.zhihu.com/p/499537362)
+[DX12渲染引擎（二）创建窗口](https://zhuanlan.zhihu.com/p/499551110)
+
